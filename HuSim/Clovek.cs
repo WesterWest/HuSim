97f84667b8969d714ccb424bfe81eddc315a4173
Jáchym Tomášek <westerofwest@gmail.com>
2017-02-16 17:59:05 +0100

    Pridana trida clovek, zakladni generace inteligence

---
+++ HuSim/Clovek.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HuSim
+{
+    class Clovek
+    {
+        private int vek;
+        private int iq;
+        private enum pohlaviEnum {muz, zena}
+
+        public Clovek(int prumerIQrodicu)
+        {
+            Random random = new Random();
+            vek = 0;
+            //pohlaviEnum pohlavi = ()random.Next(1) + 1;
+
+            //vygeneruje iq +- 20 od prumeru IQ rodicu
+            iq = random.Next(40) + prumerIQrodicu - 20;
+            //pokud je IQ mimo limit, tak vygeneruje nahodne IQ
+            if(iq > 160 || iq < 70)
+            {
+                iq = random.Next(90) + 70;
+            }
+        }
+    }
+}
---
+++ HuSim/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HuSim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*int nahoda = random.Next(4913);
+            if (nahoda % 3 == 0)
+            {
+                iq = random.Next(20) + 90;
+            }
+            else if (nahoda % 3 == 1)
+            {
+                iq = random.Next(20) + 70;
+            }
+            else
+            {
+                nahoda = random.Next(2);
+                if(nahoda == 2)
+                {
+                    iq = random.Next(30) + 130;
+                }
+                else
+                {
+                    iq = random.Next(20) + 110;
+                }
+            }
+            Pro pozdeji generovani na zacatku lidi
+             */
+        }
+    }
+}
